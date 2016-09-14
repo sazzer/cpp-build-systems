6a948d27931a9decd1a947a99f912facf8c58341
Vadim Zeitlin <vadim@zeitlins.org>
2016-09-14 22:26:34 +0200

    Fix building shared library under Windows
    Symbols must be exported from it, they're hidden by default.
    
    Notice that the same would be true if ELF visibility were used under Unix
    systems, as it should be.

--- src/shared/main/mathematics.h
+++ src/shared/main/mathematics.h
@@ -1,13 +1,35 @@
 #ifndef CPP_BUILD_SYSTEMS_SHARED_MATHEMATICS_H
 #define CPP_BUILD_SYSTEMS_SHARED_MATHEMATICS_H
 
+#ifdef _WIN32
+    #ifdef _MSC_VER
+        #define CPP_BUILD_EXPORT __declspec(dllexport)
+        #define CPP_BUILD_IMPORT __declspec(dllimport)
+    #elif defined(__GNUC__)
+        #define CPP_BUILD_EXPORT __attribute__((dllexport))
+        #define CPP_BUILD_IMPORT __attribute__((dllimport))
+    #else
+        // Hope for auto-import/export to work.
+        #define CPP_BUILD_EXPORT
+        #define CPP_BUILD_IMPORT
+    #endif
+
+    #ifdef SHARED_EXPORTS
+        #define CPP_BUILD_EXPORT_DECL CPP_BUILD_EXPORT
+    #else
+        #define CPP_BUILD_EXPORT_DECL CPP_BUILD_IMPORT
+    #endif
+#else
+    #define CPP_BUILD_EXPORT_DECL
+#endif
+
 /**
  * Add two numbers together
  * @param a The first number to add together
  * @param b The second number to add together
  * @return The result of a + b
  */
-int add(int a, int b);
+CPP_BUILD_EXPORT_DECL int add(int a, int b);
 
 /**
  * Multiply two numbers together
@@ -15,6 +37,6 @@ int add(int a, int b);
  * @param b The second number to multiply together
  * @return The result of a - b
  */
-int multiply(int a, int b);
+CPP_BUILD_EXPORT_DECL int multiply(int a, int b);
 
 #endif
